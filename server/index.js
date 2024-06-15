a3f7a0afb991e69da55a26df332476f7a9de6955
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-06-16 03:37:57 +0530

    commit on render

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const io = new Server(server, {
 
 app.use(cors());
 
-app.get("/", express.static(path.join(__dirname, "client/build")));
+app.get("/", express.static(path.join(__dirname, "../client/build")));
 
 
 const emailToSocketIdMap = new Map();
