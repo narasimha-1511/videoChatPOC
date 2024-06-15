d27ab3bc912349d8dbba8034782ad04d848a95ed
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-06-16 02:11:24 +0530

    update cors policy backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const { Server} = require('socket.io');
 
-const io = new Server(5000 , {
-    cors : {
-        origin : 'http://localhost:3000'
-    }
+const io = new Server(5000, {
+  cors: {
+    origin: "https://video-chat-poc-1xor.vercel.app/",
+  },
 });
 
 const emailToSocketIdMap = new Map();
